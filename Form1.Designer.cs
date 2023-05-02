809d7947f892b9ffb914ad30557c8f197dff3754
micaelavm <77209822+micaelavm@users.noreply.github.com>
2023-05-02 00:36:25 -0300

    Prueba chart

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.velocimetro1 = new TestGdiSpeedometerApp.GDISpeedometer();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.lblAltitude = new System.Windows.Forms.Label();
+            this.chartAltitude = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartAltitude)).BeginInit();
             this.SuspendLayout();
             // 
             // velocimetro1
@@ -39,7 +47,7 @@
             this.velocimetro1.Font = new System.Drawing.Font("Arial", 8F);
             this.velocimetro1.GaugeColor = System.Drawing.Color.Black;
             this.velocimetro1.GaugeThickness = 3;
-            this.velocimetro1.Location = new System.Drawing.Point(274, 104);
+            this.velocimetro1.Location = new System.Drawing.Point(405, 0);
             this.velocimetro1.MaxSpeed = 100D;
             this.velocimetro1.MinSpeed = 0D;
             this.velocimetro1.Name = "velocimetro1";
@@ -66,16 +74,56 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.lblAltitude);
+            this.flowLayoutPanel1.Controls.Add(this.chartAltitude);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(36, 119);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(363, 319);
+            this.flowLayoutPanel1.TabIndex = 3;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // lblAltitude
+            // 
+            this.lblAltitude.AutoSize = true;
+            this.lblAltitude.Location = new System.Drawing.Point(3, 0);
+            this.lblAltitude.Name = "lblAltitude";
+            this.lblAltitude.Size = new System.Drawing.Size(83, 13);
+            this.lblAltitude.TabIndex = 4;
+            this.lblAltitude.Text = "Payload Altitude";
+            // 
+            // chartAltitude
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartAltitude.ChartAreas.Add(chartArea1);
+            legend1.Enabled = false;
+            legend1.Name = "Legend1";
+            this.chartAltitude.Legends.Add(legend1);
+            this.chartAltitude.Location = new System.Drawing.Point(3, 16);
+            this.chartAltitude.Name = "chartAltitude";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartAltitude.Series.Add(series1);
+            this.chartAltitude.Size = new System.Drawing.Size(344, 300);
+            this.chartAltitude.TabIndex = 4;
+            this.chartAltitude.Text = "chart1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.velocimetro1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartAltitude)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +134,9 @@
         private TestGdiSpeedometerApp.GDISpeedometer velocimetro1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label lblAltitude;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartAltitude;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace Cansat2023
 {
@@ -15,11 +16,45 @@ namespace Cansat2023
         public Form1()
         {
             InitializeComponent();
+            chartAltitude.Series["Series1"].ChartType = SeriesChartType.Spline; // Tipo de grafica: Bar, Column
+            chartAltitude.Series["Series1"].BorderWidth = 2; // Borde de la grafica
+            chartAltitude.Series["Series1"].Color = Color.DarkRed; // Color de la grafica
+            //chartAltitude.Series["Series1"].LegendText; // Label o etiqueta
+            Dictionary<string, int> dic = new Dictionary<string, int>();
+            dic.Add("00:00:02", 576);
+            dic.Add("00:00:03", 687);
+            dic.Add("00:00:04", 761);
+            dic.Add("00:00:05", 860);
+            dic.Add("00:00:06", 774);
+            dic.Add("00:00:07", 576);
+            dic.Add("00:00:08", 487);
+            dic.Add("00:00:09", 261);
+            dic.Add("00:00:10", 200);
+            dic.Add("00:00:11", 174);
+            foreach (KeyValuePair<string, int> d in dic)
+            {
+                chartAltitude.Series["Series1"].Points.AddXY(d.Key, d.Value);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             velocimetro1.Speed = Convert.ToDouble(textBox1.Text);///
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
